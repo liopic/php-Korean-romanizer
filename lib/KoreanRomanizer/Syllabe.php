74a77a38a5d6f87a4636dd857848dc413188111a
Julio Martinez <liopic@liopic.com>
2015-01-14 12:26:44 +0100

    Syllabe slits Jamos and saves them

--- lib/KoreanRomanizer/Syllabe.php
+++ lib/KoreanRomanizer/Syllabe.php
@@ -1,15 +1,28 @@
 <?php
 namespace KoreanRomanizer;
 
-class Syllabe
+/**
+ * Syllabe (a Korean block with 2 or more letters)
+ */
+class Syllabe extends UnicodeChar
 {
     const FIRST_KOREAN_SYLLABE = 44032; //가, Unicode 0xAC00
     const LAST_KOREAN_SYLLABE  = 55203; //힣, Unicode 0xD7A3
 
     /**
-     * @var string sillabe
+     * @var Consonant initial consonant
      */
-    private $char;
+    private $iniConsonant;
+
+    /**
+     * @var Vowel vowel
+     */
+    private $vowel;
+
+    /**
+     * @var Consonant ending consonant
+     */
+    private $endConsonant;
 
     /**
      * @var bool Korean or not
@@ -23,12 +36,10 @@ class Syllabe
      */
     public function __construct($s)
     {
-        //Should be only an UTF-8 hangul syllabe
-        if (mb_strlen($s, "UTF-8") != 1) {
-            throw new InvalidArgumentException("The parameter of Syllabe must be a single UTF-8 character.");
-        }
+        parent::__construct($s);
         $this->char = $s;
-        $this->isKoreanFlag = self::isKoreanUTF8($s);
+        $this->isKoreanFlag = $this->isKoreanSyllabe();
+        $this->splitJamo();
     }
 
     /**
@@ -41,38 +52,58 @@ class Syllabe
     }
 
     /**
-     * Returns the unicode index of the character $s
+     * Tells if the char $s is a Korean Syllabe in UTF8
      * @param string $s
      * @return integer
      */
-    protected static function getUnicodeIndex($s)
+    private function isKoreanSyllabe()
     {
-        $long = mb_convert_encoding($s, "UCS2", "UTF-8");
-        return hexdec(bin2hex($long));
-    }
-
-    /**
-     * Protected method to tell if the char $s is a Korean Syllabe in UTF8
-     * @param string $s
-     * @return integer
-     */
-    protected static function isKoreanUTF8($s)
-    {
-        $dec = self::getUnicodeIndex($s);
+        $dec = $this->getUnicodeIndex();
         return $dec >= self::FIRST_KOREAN_SYLLABE
             && $dec <= self::LAST_KOREAN_SYLLABE;
     }
 
     /**
-     * Returns the romanization of the syllabe
+     * Extract the letters of the syllabe
      * @return string
      */
-    public function romanize()
+    public function splitJamo()
     {
         if (!$this->isKoreanFlag) {
-            return $this->char;
+            return false;
         }
 
+        $base = $this->getUnicodeIndex() - self::FIRST_KOREAN_SYLLABE;
+
+        //Extract ending consonant
+        $finals = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ",
+            "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ",
+            "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+        $finalC = count($finals);
+        $finalIndex = $base % $finalC;
+        $this->endConsonant = new Consonant($finals[$finalIndex]);
+        $base = ($base-$finalIndex) / $finalC;
+
+        //Extract vowel
+        $vowels = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ",
+            "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
+        $vowelC = count($vowels);
+        $vowelIndex = $base % $vowelC;
+        $this->vowel = new Vowel($vowels[$vowelIndex]);
+        $base = ($base-$vowelIndex) / $vowelC;
+
+        //Extract initial consonant
+        $initials = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ",
+            "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+        $initialC = count($initial);
+        $initialIndex = $base % $initialC;
+        $this->iniConsonant = new Consonant($initials[$initialIndex]);
+
+        return true;
+    }
+
+    public function romanize()
+    {
         //initial consonant romanization
         // ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ",
         //  "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
