0c0ac13bbc9d8115b741ffcc8c86b73b68f27bbf
Julio Martinez <liopic@liopic.com>
2015-01-30 10:26:37 +0100

    Removed duplication of allowed chars

--- src/KoreanRomanizer/Jamo.php
+++ src/KoreanRomanizer/Jamo.php
@@ -35,11 +35,8 @@ abstract class Jamo extends UnicodeChar implements RomanizeInterface
      */
     public static function getAllowedChars()
     {
-        $consonants = ["㄰", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ",
-            "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ",
-            "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
-        $vowels = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ",
-            "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
+        $consonants = EndConsonant::getAllowedChars();
+        $vowels     = Vowel::getAllowedChars();
         return array_merge($consonants, $vowels);
     }
 
