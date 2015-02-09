7268086dd3bc30bc985f28086b85e75c324cd5fe
Julio Martinez <liopic@liopic.com>
2015-02-09 21:02:49 +0100

    Dictionary with DictionaryEntrys and translate()

---
+++ src/KoreanRomanizer/Dictionary/Dictionary.php
@@ -0,0 +1,39 @@
+<?php
+namespace KoreanRomanizer\Dictionary;
+
+/**
+ * Container for instances of DictionaryEntry
+ */
+class Dictionary extends \SplObjectStorage
+{
+    public function attach($entry, $inf = null)
+    {
+        if ($entry instanceof DictionaryEntry) {
+            parent::attach($entry);
+        }
+    }
+
+    public function detach($entry, $inf = null)
+    {
+        if ($entry instanceof DictionaryEntry) {
+            parent::detach($entry);
+        }
+    }
+
+    /**
+     * Translate in the given string using all stored entries
+     * @param $s
+     * @return string
+     */
+    public function translate($s)
+    {
+        $this->rewind();
+        while ($this->valid())
+        {
+            $current = $this->current();
+            $s = $current->translate($s);
+            $this->next();
+        }
+        return $s;
+    }
+}
--- tests/DictionaryTest.php
+++ tests/DictionaryTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use KoreanRomanizer\Dictionary\Dictionary;
 use KoreanRomanizer\Dictionary\DictionaryEntry;
 
 class DictionaryTest extends PHPUnit_Framework_TestCase
@@ -36,5 +37,14 @@ class DictionaryTest extends PHPUnit_Framework_TestCase
         $translation = "oh";
         $d = new DictionaryEntry($pat, $translation);
         $this->assertEquals('oh(1) oh아이우', $d->translate($s));
+    }
+
+    public function testDictionaryTranslation()
+    {
+        $dict = new Dictionary();
+        $dict->attach(new DictionaryEntry("123", "321"));
+        $dict->attach(new DictionaryEntry("ab", "=AB="));
+        $result = $dict->translate('123456,abcd,321');
+        $this->assertEquals('321456,=AB=cd,321', $result);
     }
 }
